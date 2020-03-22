c251f5df189ec84bbafb60b363dd8fa94e9e4019
yrevtovich <yrevtovich@mail.ru>
2020-03-22 22:02:48 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let currentNumber;
+    if ( n >= 0) {
+        currentNumber = n.toString();
+    } else {
+        currentNumber = (0-n).toString();
+    }
+
+    let newNumber = '';
+
+    for (let i = currentNumber.length - 1; i >= 0; i--) {
+        newNumber += currentNumber[i];
+    }
+
+    return +newNumber;
 }
